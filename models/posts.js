59038eb9ad2e90c5c4fe728a0a48f9da19f435f6
AlaaAlhilly <alaa.naji.us@gmail.com>
2019-02-04 19:10:07 -0600

    CREATED THE CRUD AND SIGN IN TECHS

--- models/posts.js
+++ models/posts.js
@@ -1,11 +1,16 @@
 module.exports = function(sequelize, DataTypes) {
-  var Post = sequelize.define("Post", {
-    author: DataTypes.STRING,
-    body: DataTypes.STRING,
-    image:DataTypes.STRING,
-    vote: DataTypes.INTEGER,
-  },{
-    freezeTableName: true
-  });
+  var Post = sequelize.define(
+    "Post",
+    {
+      author: DataTypes.STRING,
+      body: DataTypes.STRING,
+      image: DataTypes.STRING,
+      vote: DataTypes.INTEGER,
+      userId: DataTypes.STRING
+    },
+    {
+      freezeTableName: true
+    }
+  );
   return Post;
 };
